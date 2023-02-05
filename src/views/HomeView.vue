146cb82fa6ee3d969428e3e42736bb12847ce812
Guillaume Lamarche <guillaumelamarche1@gmail.com>
2023-02-05 14:50:17 -0500

    removed function doc from homeview because it caused Hot reload(Hmr) errors

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,15 +3,11 @@ import { useUserStore } from '@/stores/user';
 import ActivityForm from '@/components/Activity/ActivityForm.vue';
 import ActivityList from '@/components/Activity/ActivityList.vue';
 import { useActivityStore } from '@/stores/activity';
-import { useCollection, useDocument, useFirestore } from 'vuefire';
-import { collection, doc } from '@firebase/firestore';
 import TripForm from '@/components/Trip/TripForm.vue';
 import TripList from '@/components/Trip/TripList.vue';
 import { useTripStore } from '@/stores/trip';
-
-const db = useFirestore();
-const userRef = useCollection(collection(db, 'Users'));
-const user = useDocument(doc(db, 'Users', useUserStore().user?.uid));
+import { useCollection, useDocument, useFirestore } from 'vuefire';
+import { collection, doc } from '@firebase/firestore';
 
 const trips = useTripStore().trips;
 const activities = useActivityStore().activities;
@@ -23,11 +19,5 @@ const activities = useActivityStore().activities;
         <TripList :trips="trips" />
         <ActivityForm />
         <ActivityList :activities="activities" />
-        <div>
-            <h1>User</h1>
-            <ul>
-                <li v-for="(value, key) in user">{{ key }}: {{ value }}</li>
-            </ul>
-        </div>
     </div>
 </template>
